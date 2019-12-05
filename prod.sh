4b45bc8bcfc0875fece453f384c2deef970b031e
ChandanShastri <crshastri@gmail.com>
2019-12-05 22:42:44 +0530

    Closure

--- prod.sh
+++ prod.sh
@@ -1,5 +1,7 @@
 rm -r prjs
 rm -r enc
+mkdir complied
+java -jar ./bin/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js beta_js/CPL.js
 ./bin/javascript-obfuscator ./beta_js/ --output ./enc
 mkdir prjs
 cp ./enc/beta_js/* ./prjs/
