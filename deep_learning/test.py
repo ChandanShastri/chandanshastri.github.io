f9ebd968af334d856b5ee02a544f350c87cf28d2
ChandanShastri <crshastri@gmail.com>
2020-01-18 21:56:42 +0530

    Adding Support for Installers

---
+++ deep_learning/test.py
@@ -0,0 +1,14 @@
+#print('Hello')
+from tkinter import *
+ 
+window = Tk()
+ 
+window.title("CS")
+ 
+window.mainloop()
+
+G = input("HSHS")
+
+
+#pyinstaller --name 'Chandan Shastri' --icon chandanshastri.ico  --onefile -w .\test.py
+#bin64 for file icons
