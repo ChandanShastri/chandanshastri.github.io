ddb5095e1cd5c6e046bff7ee5e1b407c8069cf67
ChandanShastri <crshastri@gmail.com>
2020-01-28 20:33:44 +0530

    Bug Fixes

--- deep_learning/getting_it_done.py
+++ deep_learning/getting_it_done.py
@@ -1,13 +1,6 @@
-print("NKDWDEKWE")
+# from functools import lru_cache
 
-def try_this () :
-    print("YES")
-a=10
-try_this()
-
-from functools import lru_cache
-
-@lru_cache(maxsize=1000)
+# @lru_cache(maxsize=1000)
 def fib(n):
     if n==1 :
         return 1
@@ -23,7 +16,7 @@ def fib_vanilla(n):
     elif n==2 :
         return 2
     elif n>2 :
-        return fib(n-1) + fib(n-2)
+        return fib_vanilla(n-1) + fib_vanilla(n-2)
 
 
 
@@ -31,17 +24,19 @@ def fib_vanilla(n):
 cache={}
 
 def fib_man(n):
+    value=0
     if n in cache :
+        #print("IN")
         return cache[n]
-    elif n==1 :
+    if n==1 :
         value = 1
     elif n==2 :
         value = 2
     elif n>2 :
-        value = fib(n-1) + fib(n-2)
+        value = fib_man(n-1) + fib_man(n-2)
     cache[n]=value
+    #print(cache)
     return value
 
-for i in range (1,100) :
+for i in range (100) :
     print(fib_man(i))
-
