8dc93784d551b10140afd2aa59ca8728bde427a4
ChandanShastri <crshastri@gmail.com>
2020-03-07 18:46:29 +0530

    Added Resume

--- bin/spark.java
+++ bin/spark.java
@@ -5,10 +5,8 @@ public class spark{
 public static void main (String args[]) {
 
 SparkSession spark = SparkSession.builder().master("local").appName("Chandan Shastri's Spark ETL Framework").getOrCreate();
-
 Scanner nc=new Scanner(System.in);
 nc.nextLine();
-
 
 }
 }
